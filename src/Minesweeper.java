967ace5ec1201fb4c9ec996d58b4b26022301975
PiotrWezyk <piotr.wezykk@gmail.com>
2017-09-11 23:22:39 +0200

    Minesweeper

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
 
 /**
- * Created by Arni on 2017-07-15.
+ In the popular Minesweeper game you have a board with some mines and those cells that don't contain a mine have a number in it that indicates the total number of mines in the neighboring cells.
+ Starting off with some arrangement of mines we want to create a Minesweeper game setup.
  */
 public class Minesweeper {
 
