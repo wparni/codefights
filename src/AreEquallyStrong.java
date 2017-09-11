8497e91094aee39adb52df81e57c5284d9e98d26
PiotrWezyk <piotr.wezykk@gmail.com>
2017-09-11 23:08:43 +0200

    AreEquallyStrong

---
+++ src/AreEquallyStrong.java
@@ -0,0 +1,16 @@
+public class AreEquallyStrong {
+    /**Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+     Call two people equally strong if their strongest arms are equally strong
+     (the strongest arm can be both the right and the left), and so are their weakest arms.
+     Given your and your friend's arms' lifting capabilities find out if you two are equally strong.*/
+
+
+    boolean areEquallyStrong(int yourLeft, int yourRight, int friendsLeft, int friendsRight) {
+
+        if(yourLeft==friendsLeft && yourRight == friendsRight || yourLeft == friendsRight && yourRight == friendsLeft){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
