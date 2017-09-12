a5968b6b64bc8502255f072addc5f753bb4b6aa6
PiotrWezyk <piotr.wezykk@gmail.com>
2017-09-12 09:17:06 +0200

    CenturyFromYear

--- src/CenturyFromYear.java
+++ src/CenturyFromYear.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+/**
+ * Given a year, return the century it is in.
+ * The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+ */
 
 public class CenturyFromYear {
+
+    int centuryFromYear(int year) {
+        if (year % 100 == 0) {
+            return year / 100;
+        } else {
+            return year / 100 + 1;
+        }
+    }
 }
