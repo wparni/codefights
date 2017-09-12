7429349acffb6b2bd37bb5d7757eadc263d6abd2
PiotrWezyk <piotr.wezykk@gmail.com>
2017-09-12 09:14:40 +0200

    Add

---
+++ src/Add.java
@@ -0,0 +1,17 @@
+/**
+ * Write a function that returns the sum of two numbers.
+ */
+
+public class Add {
+
+    int param1 = 5;
+    int param2 = 7;
+
+    int add(int param1, int param2) {
+        this.param1 = param1;
+        this.param2 = param2;
+        return param1 + param2;
+    }
+
+}
+
