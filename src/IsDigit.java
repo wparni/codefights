340dac6eb701c06e264a93b1e184ce519ceb30bc
PiotrWezyk <piotr.wezykk@gmail.com>
2017-09-12 07:32:35 +0200

    IsDigit

---
+++ src/IsDigit.java
@@ -0,0 +1,10 @@
+
+/**
+ * Determine if the given character is a digit or not.
+ */
+public class IsDigit {
+
+    static boolean isDigit(char symbol) {
+        return symbol >= '0' && symbol <= '9';
+    }
+}
